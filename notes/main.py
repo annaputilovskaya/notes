f60c0d50130b2990bedfbd27b2a59f5341f1578a
anna putilovskaya <putilovskayaam@gmail.com>
2024-11-18 09:09:59 +0300

    include routers for notes to main app

--- notes/main.py
+++ notes/main.py
@@ -1,10 +1,10 @@
 from contextlib import asynccontextmanager
 
 import uvicorn
+from core.config import settings
+from core.database import db_helper
 from fastapi import FastAPI
-
-from config.config import settings
-from config.database import db_helper
+from notices.routers import router as notes_router
 
 
 @asynccontextmanager
@@ -17,8 +17,10 @@ async def lifespan(app: FastAPI):
 
 main_app = FastAPI(title="Notes", lifespan=lifespan)
 
+main_app.include_router(notes_router)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     uvicorn.run(
-        'main:main_app', host=settings.run.host, port=settings.run.port, reload=True
+        "main:main_app", host=settings.run.host, port=settings.run.port, reload=True
     )
