6e444d0b015ef7b849c3ceb2a80deba69fc527de
anna putilovskaya <putilovskayaam@gmail.com>
2024-11-26 16:12:31 +0300

    alembic revision

--- notes/core/__init__.py
+++ notes/core/__init__.py
@@ -1,5 +1,6 @@
-__all__ = ("db_helper", "Base", "Note", "Tag", "NoteTagAssociation")
+__all__ = ("db_helper", "Base", "Note", "Tag", "NoteTagAssociation", "User")
 
 from core.base import Base
 from core.database import db_helper
 from notices.models import Note, NoteTagAssociation, Tag
+from users.models import User
