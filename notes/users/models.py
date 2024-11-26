1274f057b7ee467eea4adac4a902905ab65138d9
anna putilovskaya <putilovskayaam@gmail.com>
2024-11-26 16:36:15 +0300

    add get_db to User

--- notes/users/models.py
+++ notes/users/models.py
@@ -1,7 +1,11 @@
-from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from core import Base
 
 
 class User(Base, SQLAlchemyBaseUserTable[int]):
-    pass
+
+    @classmethod
+    def get_db(cls, session: AsyncSession):
+        return SQLAlchemyUserDatabase(session, User)
