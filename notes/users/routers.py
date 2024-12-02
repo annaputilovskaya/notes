93cf768a29d8976773afc4c2e2d07ba03a203015
anna putilovskaya <putilovskayaam@gmail.com>
2024-12-02 14:28:14 +0300

    initial routers.py for user

---
+++ notes/users/routers.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter, Depends
+from fastapi.security import HTTPBearer
+from fastapi_users import FastAPIUsers
+
+from users.auth_backend import authentication_backend
+from users.dependencies import get_user_manager
+from users.models.user import User
+from users.schemas import UserRead, UserCreate, UserUpdate
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [authentication_backend],
+)
+
+http_bearer = HTTPBearer(auto_error=False)
+
+router = APIRouter(
+    prefix="/users",
+    tags=["Пользователи"],
+    dependencies=[Depends(http_bearer)],
+)
+
+# /login
+# /logout
+router.include_router(
+    fastapi_users.get_auth_router(authentication_backend),
+)
+# /register
+router.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+)
+# /me
+# /{id}
+router.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate),
+)
