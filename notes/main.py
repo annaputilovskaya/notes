c27dd9f5cb58ba38fa2ed4b6375d5a3c87233227
anna putilovskaya <putilovskayaam@gmail.com>
2024-12-02 14:29:05 +0300

    add routers for user to main_app

--- notes/main.py
+++ notes/main.py
@@ -5,6 +5,7 @@ from core.config import settings
 from core.database import db_helper
 from fastapi import FastAPI
 from notices.routers import router as notes_router
+from users.routers import router as users_router
 
 
 @asynccontextmanager
@@ -18,6 +19,7 @@ async def lifespan(app: FastAPI):
 main_app = FastAPI(title="Notes", lifespan=lifespan)
 
 main_app.include_router(notes_router)
+main_app.include_router(users_router)
 
 
 if __name__ == "__main__":
